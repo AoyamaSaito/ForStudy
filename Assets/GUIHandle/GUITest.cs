c7ed51c8e24ef762038d807de3109d333419c0e0
aoyama_saito <saitomarimo1229@gmail.com>
2022-09-23 16:02:13 +0900

    GUITest中

---
+++ Assets/GUIHandle/GUITest.cs
@@ -0,0 +1,17 @@
+using Gamekit3D.GameCommands;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GUITest : GUIHandle
+{
+    private Vector3 _start;
+    private Vector3 _end;
+
+    private void Start()
+    {
+        _start = StartPos;
+        _end = EndPos;
+    }
+}
